536f6cbc2e9346857054aeaed03e385c795f9c05
Daniel Kennett <daniel.l.kennett@gmail.com>
2024-06-12 19:16:58 -0400

    Added assignment and data directories. Started basic main.py to load data

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # Author: Daniel Kennett
+import pandas as pd
+import numpy as np
+# import torch
+
+fama_french = pd.read_csv('data/F-F_Research_Data_5_Factors_2x3.csv')
+fama_french.rename({'Unnamed: 0':'date'}, axis=1, inplace=True)
+print(fama_french)
+
+predictor_factors = pd.read_excel('data/PredictorData2023.xlsx')
+predictor_factors.rename({'yyyymm':'date'}, axis=1, inplace=True)
+
+print(predictor_factors)
